851e4381b40b022955ff1cc969ac22fe90279478
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2020-06-25 11:37:10 +0200

    Update build-iso.sh

--- ubuntu-minimal/build-iso.sh
+++ ubuntu-minimal/build-iso.sh
@@ -79,7 +79,7 @@ d-i partman-auto/choose_recipe select root-encrypted
 d-i partman-auto/expert_recipe string                         \
       root-encrypted ::                                       \
               1024 100 1024 ext4                              \
-                      $gptonly{ }
+                      $gptonly{ }                             \
                       $primary{ } $bootable{ }                \
                       method{ format } format{ }              \
                       use_filesystem{ } filesystem{ ext4 }    \
@@ -88,14 +88,22 @@ d-i partman-auto/expert_recipe string                         \
               8192 150 -1 ext4                                \
                       $lvmok{ } lv_name{ root }               \
                       in_vg { crypt }                         \
-                      $gptonly{ }
+                      $gptonly{ }                             \
                       $primary{ }                             \
                       method{ format } format{ }              \
                       use_filesystem{ } filesystem{ ext4 }    \
                       mountpoint{ / }                         \
               .
 d-i partman/default_filesystem string ext4
+d-i partman-partitioning/no_bootable_gpt_biosgrub boolean false
+d-i partman-partitioning/no_bootable_gpt_efi boolean true
 d-i partman-basicfilesystems/no_mount_point boolean false
+d-i partman-basicfilesystems/choose_label string gpt
+d-i partman-basicfilesystems/default_label string gpt
+d-i partman-partitioning/choose_label string gpt
+d-i partman-partitioning/default_label string gpt
+d-i partman/choose_label string gpt
+d-i partman/default_label string gpt
 d-i partman-partitioning/confirm_write_new_label boolean true
 d-i partman/choose_partition select finish
 d-i partman/confirm boolean true
@@ -103,14 +111,27 @@ d-i partman/confirm_nooverwrite boolean true
 
 d-i pkgsel/updatedb boolean true
 d-i pkgsel/upgrade select full-upgrade
-#d-i pkgsel/include string openssh-server build-essential kde-plasma-desktop
-#d-i pkgsel/include string openssh-server build-essential ubuntu-gnome-desktop
-d-i pkgsel/include string openssh-server build-essential lvm2 cryptsetup
+d-i pkgsel/include string kde-plasma-desktop ubuntu-gnome-desktop
 d-i pkgsel/language-packs multiselect en, pl
 d-i pkgsel/update-policy select unattended-upgrades
 
 d-i cdrom-detect/eject boolean true
 d-i finish-install/reboot_in_progress note
+
+d-i preseed/late_command string in-target apt-get update ; \
+                                in-target apt-get upgrade -y ; \
+                                in-target apt-get dist-upgrade -y ; \
+                                in-target apt-get install -y openssh-server build-essential lvm2 cryptsetup ; \
+                                in-target apt-get install -y sudo gdebi wget perl gawk sed awk python3 ; \
+                                in-target wget https://zoom.us/client/latest/zoom_amd64.deb -O /tmp/zoom_amd64.deb ; \
+                                in-target wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.4.3-amd64.deb -O /tmp/slack-desktop-4.4.3-amd64.deb ; \
+                                in-target wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb -O /tmp/teamviewer_amd64.deb ; \
+                                in-target wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb ; \
+                                in-target dpkg -i /tmp/zoom_amd64.deb ; \
+                                in-target dpkg -i /tmp/slack-desktop-4.4.3-amd64.deb ; \
+                                in-target dpkg -i /tmp/teamviewer_amd64.deb ; \
+                                in-target dpkg -i /tmp/google-chrome-stable_current_amd64.deb ; \
+                                in-target apt-get --fix-broken install -y
 EndOfMessage
 
 chmod 644 "$TEMP_DIR/initrd/preseed.cfg"
